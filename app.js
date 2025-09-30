93c983340f9c6d2558ae8083947cf3f14aaf7217
Lucas Barbosa <barbosals298@gmail.com>
2025-09-29 21:01:27 -0300

    Aprimorando

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-const express = require("express");
+const express = require("express")
+const { engine } = require("express-handlebars")
 
-const PORT = 3000;
+const PORT = 3000
+const app = express()
+const handlebars = require("express-handlebars")
 
-const app = express();
+app.engine('handlebars', engine({defaultLayout: 'main', extname: '.handlebars'}))
+app.set('view engine', 'handlebars')
 
 app.get('/', (req, res) => {
-    res.send('<h1>Olá mundo</h1>');
+    res.render('./home', { nome: "Lucas", isLogged: true })
 })
 
 app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
+    console.log(`Servidor rodando em http://localhost:${PORT}`)
 })
