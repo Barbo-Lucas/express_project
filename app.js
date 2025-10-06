e8d3f263c1eabd1c160b7449ca70310710d3bf72
Lucas Barbosa <barbosals298@gmail.com>
2025-10-06 20:18:39 -0300

    Adicionando css e js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const { engine } = require("express-handlebars")
+const path = require('path')
 
 const PORT = 3000
 const app = express()
@@ -7,6 +8,9 @@ const handlebars = require("express-handlebars")
 
 app.engine('handlebars', engine({defaultLayout: 'main', extname: '.handlebars'}))
 app.set('view engine', 'handlebars')
+
+app.use(express.static(path.join(__dirname, 'css/')))
+app.use(express.static(path.join(__dirname, 'js/')))
 
 app.get('/', (req, res) => {
     res.render('./home', { nome: "Lucas", isLogged: true })
